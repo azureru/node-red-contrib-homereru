0756a692b4bf67e305eac67a2d5c08c73195a769
azureru <eru.saputra@gmail.com>
2017-08-05 18:25:50 +0700

    Add support for lgtv-request - but return false when failed so we can check whether the TV is on or off

---
+++ devices/lg-request.js
@@ -0,0 +1,30 @@
+module.exports = function (RED) {
+	function LgtvRequestNode(n) {
+		RED.nodes.createNode(this, n);
+		var node = this;
+		this.tv = n.tv;
+
+		this.tvConn = RED.nodes.getNode(this.tv);
+
+		if (this.tvConn) {
+			this.tvConn.register(node);
+
+			this.on('close', function (done) {
+				node.tvConn.deregister(node, done);
+			});
+
+			node.on('input', function (msg) {
+				node.tvConn.request(msg.topic, msg.payload, function (err, res) {
+					if (!err) {
+						node.send({payload: res});
+					} else {
+						node.send({payload: false});
+					}
+				});
+			});
+		} else {
+			this.error('No TV Configuration');
+		}
+	}
+	RED.nodes.registerType('lgtv-request2', LgtvRequestNode);
+};
